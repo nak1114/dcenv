db129ed18df7bd4de300714d4c6e959840bc3c1e
nak1114 <naktak1114@gmail.com>
2017-03-09 03:11:58 +0900

    Remove unused code

--- main_test.go
+++ main_test.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
-
-var message = "initial"
-var count = 0
-
-func set(mes string) {
-	message = mes
-	count = 0
-}
-func eq(t *testing.T, actual interface{}, expected interface{}) {
-	count++
-	if actual != expected {
-		fmt.Println(message, count)
-		t.Errorf("got %v\nwant %v", actual, expected)
-	}
-}
 
 func TestMain(m *testing.M) {
 	code := m.Run()
