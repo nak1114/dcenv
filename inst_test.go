be6384569911925625b7c408b0520343c5c69842
nak1114 <naktak1114@gmail.com>
2017-03-09 22:27:12 +0900

    Add test

--- inst_test.go
+++ inst_test.go
@@ -135,3 +135,130 @@ func TestYaml(t *testing.T) {
 	a.Neq(e, error(nil))
 
 }
+
+func TestSearchImageFromYard(t *testing.T) {
+	a.Set(t, "SearchImageFromYard")
+	orgExit := exit
+	exit = pseudoExit
+	flgExit = false
+	envHome = "./misc/test/dcenv_home"
+	envShell = "bash"
+	tc := Image{}
+
+	outb, _ := a.StubIO("", func() {
+		tc = SearchImageFromYard("naktak/inst-test-null", "inst-test-null", "", 0)
+	})
+	outexpect := `[naktak/inst-test-null] config data were not found in the yard.
+Please command 'dcenv yard --list naktak/inst-test-null'
+`
+	a.Eq(flgExit, true)
+	a.Eq(tc.Tag, "")
+	a.Eq(outb, outexpect)
+
+	flgExit = false
+	tc = Image{}
+	outb, _ = a.StubIO("", func() {
+		tc = SearchImageFromYard("naktak/inst-test", "inst-test-null", "", 20)
+	})
+	outexpect = `5 [naktak/inst-test] config data were found in the yard.
+Too large number you choice.:(20)
+Please command 'dcenv yard -d naktak/inst-test'
+`
+	a.Eq(flgExit, true)
+	a.Eq(tc.Tag, "")
+	a.Eq(outb, outexpect)
+
+	exit = orgExit
+
+	tc = Image{}
+	tc = SearchImageFromYard("naktak/inst-test", "inst-test", "", 0)
+	a.Eq(tc.Tag, "latest")
+	a.Eq(tc.Script, `echo "hogeid2 {{.CfgDir}} {{.Image}}:{{.Tag}} {{.Cmd}}"`+"\n")
+
+	tc = Image{}
+	tc = SearchImageFromYard("naktak/inst-test", "inst-test", "1.1.1", 3)
+	a.Eq(tc.Tag, "1.1.1")
+	a.Eq(tc.Script, `echo "hogeid3 {{.CfgDir}} {{.Image}}:{{.Tag}} {{.Cmd}}"`+"\n")
+
+	isV = true
+	tc = Image{}
+	outb, _ = a.StubIO("", func() {
+		tc = SearchImageFromYard("naktak/inst-test", "inst-test", "", 4)
+	})
+	outexpect = `5 [naktak/inst-test] config data were found in the yard.
+Invalid command. Removed: te/st2
+`
+	a.Eq(tc.Tag, "0.0.1")
+	a.Eq(tc.Script, `echo "hogeid4 {{.CfgDir}} {{.Image}}:{{.Tag}} {{.Cmd}}"`+"\n")
+	a.Eq(outb, outexpect)
+
+}
+
+//NewYardPackFromYard
+func TestNewYardPackFromYard(t *testing.T) {
+	a.Set(t, "NewYardPackFromYard")
+	orgExit := exit
+	exit = pseudoExit
+	flgExit = false
+	envHome = "./misc/test/dcenv_home"
+	envShell = "bash"
+	tc := YardPack{}
+	outexpect := ""
+
+	tc = YardPack{}
+	outb, _ := a.StubIO("", func() {
+		tc = NewYardPackFromYard("naktak/inst-test-illigal")
+	})
+	outexpect = "yaml: unmarshal errors:\n  line 52: cannot unmarshal !!str `0.0.1` into main.Image\n"
+	a.Eq(flgExit, true)
+	a.Eq(outb, outexpect)
+
+	exit = orgExit
+
+	tc = YardPack{}
+	tc = NewYardPackFromYard("naktak/inst-test-nofile")
+	a.Eq(len(tc), 0)
+
+	tc = YardPack{}
+	tc = NewYardPackFromYard("naktak/inst-test")
+	a.Eq(len(tc), 5)
+
+}
+
+//LoadConfig
+func TestLoadConfig(t *testing.T) {
+	a.Set(t, "LoadConfig")
+	cfg := &Config{}
+	outb := ""
+	isV = true
+	outb, _ = a.StubIO("", func() {
+		cfg = LoadConfig("./misc/test/dcenv_home/files/dcenv_LoadConfig_fail")
+	})
+	a.Eq(cfg, (*Config)(nil))
+	a.Eq(outb, `  File can not unmarshal.: ./misc/test/dcenv_home/files/dcenv_LoadConfig_fail
+yaml: unmarshal errors:
+  line 8: cannot unmarshal !!bool `+"`false`"+` into main.Image
+`)
+
+	outb, _ = a.StubIO("", func() {
+		cfg = LoadConfig("./misc/test/dcenv_home/files/dcenv_LoadConfig_nofile")
+	})
+	a.Eq(cfg, (*Config)(nil))
+	a.Eq(outb, `  File can not unmarshal.: ./misc/test/dcenv_home/files/dcenv_LoadConfig_nofile
+open ./misc/test/dcenv_home/files/dcenv_LoadConfig_nofile: The system cannot find the file specified.
+`)
+
+	outb, _ = a.StubIO("", func() {
+		cfg = LoadConfig("./misc/test/dcenv_home/files/dcenv_LoadConfig")
+	})
+	a.Neq(cfg, (*Config)(nil))
+	a.Eq(outb, ``)
+	isV = false
+
+	outb, _ = a.StubIO("", func() {
+		cfg = LoadConfig("./misc/test/dcenv_home/files/dcenv_LoadConfig_fail")
+	})
+	a.Eq(cfg, (*Config)(nil))
+	a.Eq(outb, ``)
+
+}
