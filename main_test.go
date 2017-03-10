a969bf2316d268e331eed57e1c8a543d6ac1212c
nak1114 <naktak1114@gmail.com>
2017-03-11 00:20:37 +0900

    Add helper function for mock

--- main_test.go
+++ main_test.go
@@ -3,12 +3,34 @@ package main
 import (
 	"os"
 	"testing"
+
+	a "github.com/nak1114/goutil/assert"
 )
 
-var flgExit = false
-
-func pseudoExit(n int) {
-	flgExit = true
+func PseudoExit(fn func(), expectedCode int, expectedCount int) (code int, count int) {
+	code = 0
+	count = 0
+	orgObj := exit
+	exit = func(n int) {
+		if count == 0 {
+			code = n
+		}
+		count++
+	}
+	fn()
+	exit = orgObj
+	a.Eq(code, expectedCode)
+	a.Eq(count, expectedCount)
+	return
+}
+func PseudoPrompterYN(pb bool, fn func()) {
+	orgObj := prompterYN
+	prompterYN = func(n string, b bool) bool {
+		return pb
+	}
+	fn()
+	prompterYN = orgObj
+	return
 }
 
 func TestMain(m *testing.M) {
